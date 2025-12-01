48e992b6d48c04c6dc630806023df066fff42daa
tranaMil <trana.2171235@studenti.uniroma1.it>
2025-12-01 19:00:41 +0100

    Forzo aggiornamento Player e GameObject mancanti

--- src/model/gameobject/GameObject.java
+++ src/model/gameobject/GameObject.java
@@ -1,9 +1,9 @@
 package model.gameobject;
 
-public class GameObject 
+public abstract class GameObject 
 {
-	int x, y, w, h;
-	int type;
+	protected int x, y, w, h;
+	protected int type;
 	
 	@Override
 	public String toString()
@@ -15,4 +15,59 @@ public class GameObject
 		this.x = x; this.y = y;
 		this.w = w; this.h = h;
 	}
+
+	public abstract void update();
+
+	public boolean intersects(GameObject other){
+		return this.x < other.x +other.w &&
+			this.x + this.w > other.x &&
+			this.y < other.y + other.h &&
+			this.y + this.h > other.y;
+	}
+
+	public boolean intersects(int otherX, int otherY, int otherW, int otherH){
+		return this.x < otherX +otherW &&
+			this.x + this.w > otherX &&
+			this.y < otherY + otherH &&
+			this.y + this.h > otherY;
+	}
+
+	public int getX(){return x;}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getW() {
+		return w;
+	}
+
+	public void setW(int w) {
+		this.w = w;
+	}
+
+	public int getH() {
+		return h;
+	}
+
+	public void setH(int h) {
+		this.h = h;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+//fix git
 }
--- src/model/gameobject/Player.java
+++ src/model/gameobject/Player.java
@@ -2,5 +2,68 @@ package model.gameobject;
 
 public class Player extends GameObject  
 {
+    private static final int PLAYER_WIDTH = 32;//non so quanto sia di preciso, da modificare
+    private static final int PLAYER_HEIGHT = 64;//uguale 
 
+    public static final int TYPE_PLAYER = 999;
+
+    //per le animazioni 
+    public enum Direction {LEFT, RIGHT};
+    public enum State {IDLE, WALKING, SEARCHING, JUMPING, FALLING};
+
+    private double velX;
+    private double velY;
+    private double speed = 3.0; 
+
+    private Direction currentDirection;
+    private State currentState;
+
+    public Player(int startX, int startY){
+        super(TYPE_PLAYER, startX, startY, PLAYER_WIDTH, PLAYER_HEIGHT);
+
+        this.velX = 0;
+        this.velY = 0;
+        this.currentDirection= Direction.RIGHT;
+        this.currentState = State.IDLE;
+
+    }    
+
+    @Override
+    public void update(){
+        x+=(int)velX;
+        y+=(int)velY;
+
+        updateAnimationsState();
+    }
+
+    private void updateAnimationsState(){
+        if (velX != 0) {
+        currentDirection = (velX > 0) ? Direction.RIGHT : Direction.LEFT;
+        }
+
+        if(velY != 0){
+            currentState = (velY > 0) ? State.FALLING : State.JUMPING;
+            return;            
+        }
+
+        if (velX != 0){currentState = State.WALKING;}
+        else{currentState = State.IDLE;}
+    }
+
+    public void setVelX(double velX){this.velX = velX;}
+
+    public void setVelY(double velY){this.velY = velY;}
+
+    public double getVelX(){ return velX; }
+    public double getVelY(){ return velY; }
+
+    public double getSpeed(){return speed;}
+    public void setSpeed(double speed){this.speed = speed;}
+
+    public Direction getDirection(){ return currentDirection; }
+    public State getState() {return currentState;}
+
+    public void setState(State s) {this.currentState = s;}
+    public void setDirection(Direction d) {this.currentDirection = d;}
+//fix git
 }
