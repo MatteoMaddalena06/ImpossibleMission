ebe410bd23978b8ab3b285e6c4536e68be173688
Matteo Maddalena <d.maddalena69@gmail.com>
2025-12-11 11:30:59 +0100

    Add some classes for debugging

--- src/model/gameobject/GameObjectFactory.java
+++ src/model/gameobject/GameObjectFactory.java
@@ -2,6 +2,9 @@ package model.gameobject;
 
 //inproject import
 import model.room.RoomMap;
+import model.utils.Point;
+import model.gameobject.enemy.BlackOrb;
+import model.gameobject.enemy.RunnerRobot;
 
 public abstract class GameObjectFactory 
 {	
@@ -13,7 +16,7 @@ public abstract class GameObjectFactory
 			case RoomMap.WALL_ID      -> new FixedObject(FixedObject.Type.WALL, position, width, height);
 			case RoomMap.FLOOR_ID     -> new FixedObject(FixedObject.Type.FLOOR, position, width, height);
 			case RoomMap.PLATFORM_ID  -> new Platform(position, width, height);
-			case RoomMap.ROBOT_ID     -> new Robot(position, width, height);
+			case RoomMap.ROBOT_ID     -> new RunnerRobot(position, width, height);
 			case RoomMap.FURNITURE_ID -> new Furniture(position, width, height, Furniture.Type.RANDOM);
 			case RoomMap.TERMINAL_ID  -> new Terminal(position, width, height);
 			case RoomMap.BLACK_ORB_ID -> new BlackOrb(position, width, height);
